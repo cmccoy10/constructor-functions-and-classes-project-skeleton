a7a7f8fb1691eadfc0c3cefaa0db64cd8db35828
Michael Jensen <mcjensen24@gmail.com>
2020-08-11 15:50:07 -0400

    finished 4

--- problems/04-class-instance-method.js
+++ problems/04-class-instance-method.js
@@ -21,9 +21,21 @@ Node.js with the examples below. Use the command:
 
 `node problems/04-class-instance-method.js`
 
-Examples:
+***********************************************************************/
+class Email {
+  constructor (recipient, sender, subject, text) {
+    this.recipient = recipient;
+    this.sender = sender;
+    this.subject = subject;
+    this.text = text;
+  }
+  getSubjectAndText() {
+    return `${this.subject}: ${this.text}`;
+  }
+}
 
-const message1 = new Email('sally@smith.com', 'john@smith.com', 
+
+const message1 = new Email('sally@smith.com', 'john@smith.com',
   'Test Message One', 'This is a test message.');
 console.log(message1);
 console.log(message1.getSubjectAndText());
@@ -38,7 +50,7 @@ console.log(message1.getSubjectAndText());
 // }
 // Test Message One: This is a test message.
 
-const message2 = new Email('sally@smith.com', 'john@smith.com', 
+const message2 = new Email('sally@smith.com', 'john@smith.com',
   'Test Message Two', 'This is a test message.');
 console.log(message2);
 console.log(message2.getSubjectAndText());
@@ -52,9 +64,6 @@ console.log(message2.getSubjectAndText());
 //   text: 'This is a test message.'
 // }
 // Test Message Two: This is a test message.
-
-***********************************************************************/
-
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
